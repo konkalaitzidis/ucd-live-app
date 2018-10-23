1a614e1c59fc0cf8f1aad8124107f39abccfc1f1
lrgazzella <lrgazzella@gmail.com>
2018-10-23 23:56:23 +0100

    Added floating add button on the MainActivity

--- app/src/main/java/com/example/ucdlive/MainActivity.java
+++ app/src/main/java/com/example/ucdlive/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.ucdlive;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.add_event_button);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addEvent(view);
+            }
+        });
     }
 
     @Override
@@ -43,5 +54,9 @@ public class MainActivity extends AppCompatActivity {
     private void goToSettingsActivity(){
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
+    }
+
+    private void addEvent(View v){
+        Toast.makeText(getBaseContext(), "Button pressed", Toast.LENGTH_LONG).show();
     }
 }
